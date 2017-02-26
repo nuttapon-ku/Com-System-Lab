65238ea61da8a8c96c3b313995c391f48d2c91f5
Nuttapon <nuttapon.l@ku.th>
2017-02-26 21:48:42 +0700

    fix bug

--- week04/3.1/testlibwrapper.py
+++ week04/3.1/testlibwrapper.py
@@ -1,3 +1,3 @@
 import ctypes
-testlib = ctypes.CDLL('/home/win/Desktop/Lab-Com-System/week04/sneakpeak.so')
+testlib = ctypes.CDLL('/home/win/Desktop/Lab-Com-System/week04/cross_matrix_100.so')
 testlib.myprint()
